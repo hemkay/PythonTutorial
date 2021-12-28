a81847d120fc51d4b201f00867c1dcc354a35934
Ibrahim Mukaila <imukaila@infinion.co>
2021-12-28 23:30:36 +0100

    class on datatype

---
+++ datatype.py
@@ -0,0 +1,54 @@
+"""
+This file is a discussion 
+about inbuilt data type in python
+
+"""
+
+
+
+# This is an example of text
+x = "Hello World"
+print(x)
+print(type(x))
+
+y = '12345'
+print(y)
+print(type(y))
+
+
+#examples of numeric data type
+#example of integer
+z = 3000
+print(z)
+print(type(z))
+
+#eaxample of float
+w = 1.2
+print(w)
+print(type(w))
+
+
+q = 1.0
+print(q)
+print(type(q))
+
+
+# example of boolean  data type
+
+r = True
+e = False
+
+print(r)
+print(type(r))
+
+print(e)
+print(type(e))
+
+
+
+
+# Example 1
+t = 20
+s = 40
+# find 2t + s
+print(2 * t + s)
